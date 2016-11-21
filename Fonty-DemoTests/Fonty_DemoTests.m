b7bacf50af6a01b8706710dfea3e24554f791aaf
颜为晨 <417495814@qq.com>
2016-11-21 18:23:15 +0800

    travis ci

---
+++ Fonty-DemoTests/Fonty_DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  Fonty_DemoTests.m
+//  Fonty-DemoTests
+//
+//  Created by 颜为晨 on 21/11/2016.
+//  Copyright © 2016 s2mh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Fonty_DemoTests : XCTestCase
+
+@end
+
+@implementation Fonty_DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
